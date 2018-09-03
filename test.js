ed1b5d847416892ecb6342e3984d3133383c0b04
DjordyKoert <33036133+DjordyKoert@users.noreply.github.com>
2018-09-03 14:54:37 +0200

    js

---
+++ test.js
@@ -0,0 +1,29 @@
+function Rnumber() {
+    var result = prompt("enter getal:");
+    var R = Math.floor(Math.random() * 10);
+    if (!result) return result = prompt("Geen nummer! \n enter getal:");
+    if (isNaN(result)) return result = prompt("Geen nummer! \n enter getal:");
+    result = parseInt(result);
+/*    var Rbool;
+    var Lijst_ding = {
+        Jij: "Moet dit begrijpen",
+        Ik: "Heb dit gemaakt #tijdverspillen",
+        Windows: "is aanstichter van alle problemen"
+    }
+    if (R < 0.4) Rbool = true;
+    if (R > 0.8) Rbool = true; */
+    if (result == R) window.alert("Lucky Number: " + R + "\n YOU WIN!");
+    if (result !== R) window.alert("Lucky Number: " + R + "\n YOU LOSE!");
+/*    switch (Rbool) {
+        case true:
+            window.alert("Probeer dit te begrijpen :D");
+            window.alert(Lijst_ding.Jij);
+            window.alert(Lijst_ding.Ik);
+            Lijst_ding.Windows.length;
+            break;
+        case false:
+            window.alert("Begrijp goed alstu. p.s. deze switch case is stom.");
+            window.alert(Lijst_ding.Windows);
+            break;
+    } */
+}
