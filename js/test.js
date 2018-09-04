dfe0f2655d23570cabd1bb4954d8ed7292753435
DjordyKoert <33036133+DjordyKoert@users.noreply.github.com>
2018-09-04 14:17:35 +0200

    mooie animatie

--- js/test.js
+++ js/test.js
@@ -1,31 +1,14 @@
 function Rnumber() {
     var result = prompt("enter getal:");
     var R = Math.floor(Math.random() * 10);
+    if (result === null) return window.alert("Je bent stom >:(");
     if (!result) return Rnumber();
     if (isNaN(result)) return Rnumber();
+    if (result > 10 && result !=666) return window.alert("Ben je dom ofzo?");
+    if (result == 666) return window.alert("3Spoopy5Me");
     result = parseInt(result);
-/*    var Rbool;
-    var Lijst_ding = {
-        Jij: "Moet dit begrijpen",
-        Ik: "Heb dit gemaakt #tijdverspillen",
-        Windows: "is aanstichter van alle problemen"
-    }
-    if (R < 0.4) Rbool = true;
-    if (R > 0.8) Rbool = true; */
     if (result == R) {
         window.alert("Lucky Number: " + R + "\n YOU WIN!");
     }
     if (result !== R) window.alert("Lucky Number: " + R + "\n YOU LOSE!");
-/*    switch (Rbool) {
-        case true:
-            window.alert("Probeer dit te begrijpen :D");
-            window.alert(Lijst_ding.Jij);
-            window.alert(Lijst_ding.Ik);
-            Lijst_ding.Windows.length;
-            break;
-        case false:
-            window.alert("Begrijp goed alstu. p.s. deze switch case is stom.");
-            window.alert(Lijst_ding.Windows);
-            break;
-    } */
 }
