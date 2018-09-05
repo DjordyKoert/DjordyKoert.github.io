eda8543596dff036f6b5a5293c30ba896dd16067
DjordyKoert <33036133+DjordyKoert@users.noreply.github.com>
2018-09-05 09:40:46 +0200

    js af + google verify

--- js/test.js
+++ js/test.js
@@ -8,31 +8,33 @@ function Rnumber() {
     if (result == 666) return window.alert("3Spoopy5Me");
     result = parseInt(result);
     if (result == R) {
-        var wins = GetCookie("wins")
-        if (!wins) document.cookie = ("wins=1; expires=Thu, 25 Dec 2018 12:00:00 UTC;")
+        var wins = parseInt(getCookie("wins"));
+        if (!wins || wins == null) {
+            document.cookie = ("wins=1; expires=Thu, 25 Dec 2018 12:00:00 UTC;");
+            console.log("Wins bestaat niet");
+        }
         else {
-            wins = parseInt(wins);
             wins = wins + 1;
-            window.alert("Lucky Number: " + R + "\n YOU LOSE! \n Total wins: " + wins)
-            wins = toString(wins);
+            window.alert("Lucky Number: " + R + "\n YOU WIN! \n Total wins: " + wins)
+            wins = wins.toString();
             document.cookie = ("wins="+ wins + "; expires=Thu, 25 Dec 2018 12:00:00 UTC;")
         }
     }
     if (result !== R) window.alert("Lucky Number: " + R + "\n YOU LOSE!");
 }
 
-function GetCookie(cwins) {
-    var wins = cwins + "=";
-    var DecodeCookie = decodeURIComponent(document.cookie);
-    var ca = DecodeCookie.split(";");
+function getCookie(cwins) {
+    var wins = cwins + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
     for(var i = 0; i <ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+        if (c.indexOf(wins) == 0) {
+            return c.substring(wins.length, c.length);
         }
     }
-    return "";
+    return '';
 }
